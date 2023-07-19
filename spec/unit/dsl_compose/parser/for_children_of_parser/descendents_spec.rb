07df76add74b3b1633c421e2a2dda419c3e3ea02
Craig Ulliott <craigulliott@gmail.com>
2023-07-18 23:25:17 -0600

    fix: for_children_of returns classes with dependencies before classes without any dependencies, this fixes a bug where the parser is used to create other classes, and classes with dependencies need to be created first

--- spec/unit/dsl_compose/parser/for_children_of_parser/descendents_spec.rb
+++ spec/unit/dsl_compose/parser/for_children_of_parser/descendents_spec.rb
@@ -6,55 +6,78 @@ RSpec.describe DSLCompose::Parser::ForChildrenOfParser::Descendents do
   before(:each) do
     # note, these are deliberately created in a non natural order, so we can
     # properly test the sorting
-    create_class "BaseClass3"
-    create_class "BaseClass1"
-    create_class "BaseClass2"
+    create_class "BaseClass"
 
-    create_class "BaseClass1::Child1", BaseClass1
+    create_class "BaseClass3", BaseClass
+    create_class "BaseClass1", BaseClass
+    create_class "BaseClass2", BaseClass
 
-    create_class "BaseClass1::Child1::Grandchild1", BaseClass1::Child1
+    create_class "BaseClass1::Child1", BaseClass
+    create_class "BaseClass1::Adult1", BaseClass
+    create_class "BaseClass1::Admin1", BaseClass1::Adult1
 
-    create_class "BaseClass2::Child1", BaseClass2
+    create_class "BaseClass1::Child1::Grandchild1", BaseClass
 
-    create_class "BaseClass1::Child2", BaseClass1
+    create_class "BaseClass2::Child1", BaseClass
 
-    create_class "BaseClass3::Child1", BaseClass3
+    create_class "BaseClass1::Child2", BaseClass
 
-    create_class "BaseClass1::Child2::Grandchild2", BaseClass1::Child2
+    create_class "BaseClass3::Child1", BaseClass
 
-    create_class "BaseClass1::Child2::Grandchild1", BaseClass1::Child2
+    create_class "BaseClass1::Child2::Grandchild2", BaseClass
 
-    create_class "BaseClass1::Child2::Grandchild2::GreatGrandhild1", BaseClass1::Child2::Grandchild2
+    create_class "BaseClass1::Child2::Grandchild1", BaseClass
+
+    create_class "BaseClass1::Child2::Grandchild2::GreatGrandhild1", BaseClass
   end
 
   describe :initialize do
     it "initializes the class without error" do
       expect {
-        DSLCompose::Parser::ForChildrenOfParser::Descendents.new BaseClass1, true
+        DSLCompose::Parser::ForChildrenOfParser::Descendents.new BaseClass, true
       }.to_not raise_error
     end
   end
 
   describe :classes do
     describe "when final_children_only is true" do
-      let(:descendants) { DSLCompose::Parser::ForChildrenOfParser::Descendents.new BaseClass1, true }
+      let(:descendants) { DSLCompose::Parser::ForChildrenOfParser::Descendents.new BaseClass, true }
 
       it "returns the expected classes in the expected order" do
         expect(descendants.classes).to eql([
+          BaseClass1,
+          BaseClass2,
+          BaseClass3,
+          BaseClass1::Admin1,
+          BaseClass1::Child1,
+          BaseClass1::Child2,
+          BaseClass2::Child1,
+          BaseClass3::Child1,
           BaseClass1::Child1::Grandchild1,
           BaseClass1::Child2::Grandchild1,
+          BaseClass1::Child2::Grandchild2,
           BaseClass1::Child2::Grandchild2::GreatGrandhild1
         ])
       end
     end
 
     describe "when final_children_only is false" do
-      let(:descendants) { DSLCompose::Parser::ForChildrenOfParser::Descendents.new BaseClass1, false }
+      let(:descendants) { DSLCompose::Parser::ForChildrenOfParser::Descendents.new BaseClass, false }
 
       it "returns the expected classes in the expected order" do
         expect(descendants.classes).to eql([
+          BaseClass1,
+          BaseClass2,
+          BaseClass3,
+          # note that Adult normally comes after Admin (alphabetically), but
+          # because Admin is a child of Adult (Adult has dependencies), it
+          # comes first
+          BaseClass1::Adult1,
+          BaseClass1::Admin1,
           BaseClass1::Child1,
           BaseClass1::Child2,
+          BaseClass2::Child1,
+          BaseClass3::Child1,
           BaseClass1::Child1::Grandchild1,
           BaseClass1::Child2::Grandchild1,
           BaseClass1::Child2::Grandchild2,
